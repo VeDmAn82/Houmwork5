7fa76beb2dc3eace0ce972e64a1318fd20b926ae
Dmitry Vechkhayzer <dmitry.v82@mail.ru>
2022-09-05 20:24:08 +0400

    add decision Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Write("Enter length Massive - ");
+bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+if (!isNumber || length <= 0)
+{
+    Console.WriteLine("Invalid number");
+    return;
+}
+
+int[] array = GetRandomArray(length);
+PrintArray(GetRandomArray(length));
+Console.WriteLine();
+Console.Write("Quantity Even Numbers: ");
+Console.WriteLine(GetCountEvenNum(array));
+
+int[] GetRandomArray(int length)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+    return array;
+}
+
+int GetCountEvenNum(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+void PrintArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");       
+    }
+    
+}
